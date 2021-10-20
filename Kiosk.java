7205d02050677ef46f9b87066b48dfce796b64c3
vijmanan04 <75040859+vijmanan04@users.noreply.github.com>
2021-10-20 14:34:09 -0400

    Original Kiosk.java from quiz; took 5 min 1 sec to type

--- Kiosk.java
+++ Kiosk.java
@@ -1,2 +1,29 @@
 
 //your main interface for the project, with the main method
+
+/*
+******** Took 5 min and 1 second to type this class
+*/
+import java.util.Scanner;
+public class Kiosk{
+  public static void main(String args[]){
+    String[] items = {"apple", "banana", "pear", "strawberry", "mango"};
+    System.out.println("Welcome to Kroger! You will purchase then buy your items");
+    double[] prices = {5.33, 7.89, 10.12, 11.15,15.34};
+    System.out.println("Please purchase your good. You will be prompted to buy a type of food, along with its price. You will need to enter the quantity you want to buy.");
+    String buffer; // clear buffer
+    Scanner scan = new Scanner(System.in); // Scanner
+    int[] qty = {0,0,0,0,0}
+    int purchaseQty;
+    double totalPrice;
+    for (int i = 0; i < 5; i++) { // ask and update quantities
+      System.out.println("How many " + items[i] + " will you like to buy? (enter an integer)");
+      purchaseQty = scan.nextInt();
+      buffer = scan.nextLine();
+      qty[i] = purchaseQty;
+      totalPrice += prices[i] * qty[i];
+    }
+    System.out.println("Great! Here is your recipt.");
+    Printer printer = new Printer();
+  } // close main
+} // close class
