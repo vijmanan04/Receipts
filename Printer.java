6606524399d05fb8bb37d52507b75cb4a01bdf89
vijmanan04 <75040859+vijmanan04@users.noreply.github.com>
2021-10-22 09:21:34 -0400

    Description of my process
    To start off, I needed to fix my syntax errors from the quiz, which I did and consisted mainly of semi-colon errors. 
    I started to continue the logic for the printOrder() function that I had left on. My thought process in making this function
    was that I need to first use math to calculate how much space is left, and then use a for-loop to create that many space. I realized
    that I needed to use a for-loop because the lenght of the food in the inventory changed. After making the changes to the previous function, I 
    made the printTotal() function that used very simillar logic to the printOrder() logic. However, I realized that, in this case, we knew the maximum length
    of the descriptor words, meaning 'total', 'subtotal', and 'tax', which was 8 due to the 'subtotal' being the longest. What I saw that I could do was make the
    smaller sized words equal in string length to subtotal by adding spaces in the string to get to 8 total characters. Now since all the descriptors have the same 
    length, I just needed to program a constant number of spaces. Lastly, since the prices could be 4 or 5 charactes long (including decimal point), I used condtionals
    to format the numbers so that an extra space was made in front of the prices with 4 characters, to make them 5. In general, my process invovled a lot of checking
    the alignment of the printer and finding ways to break down the complexity into smaller parts that allowed me to slowly tackle the problem.

--- Printer.java
+++ Printer.java
@@ -1,61 +1,118 @@
-//Printer.java
-
-/* This is a java file 'stub' or piece of a file designed to get you started on 
-* learning to code in java, specifically with multiple classes and methods that rely on the other.
-*
-*For this assignment, I would like you to fork, modify and commit this project, while
-* adding the following elements to your code.
-*
-* The receipt printer file could take arguments or parametrs from another class
-* This file will be purely for output
-****************************************
-* 40 characters is a standard width    * 
-* for typical receipt paper, your task *
-* is to make output from the screen    *
-* look like it is printing to a small  *
-* receipt type printer, with all the   *
-* text formatted to 40 characters wide *
-*                                      *
-* Have fun with the assignment!!       *
-****************************************
-*/
-
-
-
 /*
 ******** Took 5 min and 34 second to type this class
+Description of my process 
+To start off, I needed to fix my syntax errors from the quiz, which I did and consisted mainly of semi-colon errors. 
+I started to continue the logic for the printOrder() function that I had left on. My thought process in making this function
+was that I need to first use math to calculate how much space is left, and then use a for-loop to create that many space. I realized
+that I needed to use a for-loop because the lenght of the food in the inventory changed. After making the changes to the previous function, I 
+made the printTotal() function that used very simillar logic to the printOrder() logic. However, I realized that, in this case, we knew the maximum length
+of the descriptor words, meaning 'total', 'subtotal', and 'tax', which was 8 due to the 'subtotal' being the longest. What I saw that I could do was make the
+smaller sized words equal in string length to subtotal by adding spaces in the string to get to 8 total characters. Now since all the descriptors have the same 
+length, I just needed to program a constant number of spaces. Lastly, since the prices could be 4 or 5 charactes long (including decimal point), I used condtionals
+to format the numbers so that an extra space was made in front of the prices with 4 characters, to make them 5. In general, my process invovled a lot of checking
+the alignment of the printer and finding ways to break down the complexity into smaller parts that allowed me to slowly tackle the problem.
 */
 public class Printer { //prints recipt
+  
+  // initialize the necessary quantities that need to be printed
   double subtotal;
   double[] prices = new double[5];
   int[] qty = new int[5];
   String[] itemNames = new String[5];
+  double tax = 0.07;
+
+
   public Printer(double totalPrice, int[] qtys, String[] items, double[] price){ //constructor
     subtotal = totalPrice;
     prices = price;
     qty = qtys;
-    itemNames = items;}
+    itemNames = items;
+  }
 
-    int spaceLeft = 0;
-    public void printOrder(){ //Prints only order without subtotals
-      System.out.println("Kroger");
-      System.out.println("Qty  \t\t\tItem  \t\t\t\tPrice");
-      for (int i = 0; i < 5; i++){
-        System.out.print(qty[i] + "\t");
-        System.out.print(itemNames[i]);
-        spaceLeft = 29 - itemNames[i].lenght();
-        for (int j = 0; j < 5; j++){
-          System.out.print(" ");
-        }
-        if (prices[i] < 10.0) { //checks for number of characters
-          System.out.print
-          /*
-            ******* This is where I stopped when the quiz ended;
-          */
-        }
+  public void printHeader(){ // prints the header for the receipt
+    for(int i = 0; i < 40; i++){ // 40 char output
+      System.out.print("*");
+    }
+    System.out.println("");
 
 
+  }
+
+  int spaceLeft = 0; // counts how much space is left since amount of space remaining changes
+  public void printOrder(){ //Prints only order without subtotals
+    System.out.println("Kroger");
+    System.out.println("Qty              Item              Price"); // 40 char output 
+    // 3 + 2 + (3 * 4) + 4 + 2 + (3 * 4) + 5 = 40 char
+    for (int i = 0; i < 5; i++){
+      System.out.print(qty[i] + "                "); // 1 + 3 + (3 * 4) = 16 char
+      System.out.print(itemNames[i]);
+
+      spaceLeft = 17 - itemNames[i].length(); // 40 char - 17 char - 5 char - 1 char - length of name (for dollar sign) =
+      for (int j = 0; j < spaceLeft; j++){
+        System.out.print(" ");
+      }
+      System.out.print("$"); // print dollar sign before each number
+
+      if (prices[i] < 10.0) { //checks for number of characters
+        System.out.print(" " + prices[i]); // prints out 5 chars
+      }
+      if (prices[i] >= 10.0) { //checks for number of characters
+        System.out.print(prices[i]); // prints out 5 chars
+      }
+      System.out.println(""); // adds a new line after each entry in purchase summary
     } // close for loop
+    System.out.println(""); // adds a new line once to give space between subtotal and purchase summary
+  } // close function
+
+  public void printTotals(){ // print total values like subtotal, tax, and total
+	for (int i = 0; i <  34; i++){
+		System.out.print(" "); // print spacing for the dashes
+	}
+	System.out.println("______"); // add dashes to split the totals from the subtotals
+    for (int i = 0; i < 19; i++){ // 40 - 8 - 7 - 1 - 5 char printed here for spaces
+      System.out.print(" "); // print calculated spaces
+    }
+    System.out.print("Subtotal      $"); //15 chars printed here
+    if (subtotal < 10.0) { //checks for number of characters
+      System.out.print(" " + String.format("%.2", subtotal)); // prints out 5 chars
+    }
+    if (subtotal >= 10.0) { //checks for number of characters
+      System.out.print(String.format("%.2f", subtotal)); // prints out 5 chars
+    }
+
+    System.out.println(""); // add a line between subtotal and tax
 
 
+	double taxPayed = subtotal * tax; // variable to hold tax on food
+    for (int i = 0; i < 19; i++){ // 40 - 8 - 7 - 1 - 5 char printed here for spaces
+      System.out.print(" "); // print calculated spaces
+    }
+    System.out.print("Tax 7%        $ "); // 12 char printed here
+    if ((taxPayed) < 10.0) { //checks for number of characters
+      System.out.print(String.format(" " + "%.2f", (subtotal * 0.07))); // format to two decimal places; print tax
+    }
+    if ((taxPayed )>= 10.0) { //checks for number of characters
+      System.out.print(String.format("%.2f", (subtotal * 0.07))); // format to two decimal places; print tax
+    }
+
+    System.out.println(""); // add a line between tax and total
+
+	double tax = 0.07;
+    for (int i = 0; i < 19; i++){ // 40 - 8 - 7 - 1 - 5 char printed here for spaces
+      System.out.print(" ");
+    }
+    System.out.print("Total         $"); // 11char printed here
+    if (subtotal + taxPayed < 10.0) { //checks for number of characters
+      System.out.print(String.format(" " + "%.2f", (subtotal + subtotal * 0.07)) + "|"); // format to two decimal places; print total
+    }
+    
+     if (subtotal + taxPayed >= 10.0) { //checks for number of characters
+      System.out.print(String.format("%.2f", (subtotal + subtotal * 0.07))); // format to two decimal places; print total
+    }
+
+    System.out.println(""); // pleasing to the eye 
+
+
+
+  } // close printTotal() function
 }// close class
